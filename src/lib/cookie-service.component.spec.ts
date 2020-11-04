30c8821bed5616143ba9e5557e7b8e648a10c454
Attila Németh <attila.nemeth@ubgnet.de>
2020-11-04 10:50:21 +0100

    First Commit

---
+++ src/lib/cookie-service.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CookieServiceComponent } from './cookie-service.component';
+
+describe('CookieServiceComponent', () => {
+  let component: CookieServiceComponent;
+  let fixture: ComponentFixture<CookieServiceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CookieServiceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CookieServiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/lib/cookie-service.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-cookie-service',
+  template: `
+    <p>
+      cookie-service works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CookieServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
